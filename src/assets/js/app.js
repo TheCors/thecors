6e9f6ce9ae49891d50697595e69652c05f3de70a
Chris Lusted <kipl20@hotmail.com>
2016-12-03 18:56:24 +0000

    add loading screen for site

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,8 @@
 $(document).foundation();
+$(window).load(function() {
+  // Animate loader off screen
+  setTimeout(function() {
+    $( "html" ).removeClass("loading");
+    console.log("removing");
+  }, 3000);
+});
